e4efe304ab60ca7d53d523b4885b88aa1b9da198
Steve Bazemore <steve@nerdmedia.com>
2019-02-22 16:29:03 -0500

    initial commit

---
+++ src/Query.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace nerdmedia\NmiQuerySdk;
+
+class Query
+{
+    public function __construct()
+    {
+        //
+    }
+
+    public function setType($type)
+    {
+        //
+    }
+
+    public function setQueryString($string)
+    {
+        //
+    }
+
+    public function run()
+    {
+        //
+    }
+}
